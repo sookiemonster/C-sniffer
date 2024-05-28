35491073b012de20bfc8d4a11c6c8f63edfa6a3a
sookiemonster <sookiemonster216@gmail.com>
2024-05-28 14:10:03 -0400

    Add Task 1.1B Q2: Capture target subnet packets

--- lab.py
+++ lab.py
@@ -4,13 +4,24 @@
 from scapy.all import *
 
 # TASK 1.1B
-# SRC:str = "255.255.255.0"
+QUESTION = 2
 
-# Sniff exclusively ICMP packet
-# sniff(filter='icmp', prn = lambda x : x.summary())
+def summarize(pkt):
+   return pkt.summary()
 
-# Sniff TCP pckets from a specified IP and dest. port of 23
-SRC:str = "255.255.255.255"
-PORT:str = "0000"
-print(f"Sniffing TCP packets from {SRC} destined for port {PORT}")
-sniff(filter=f"tcp and src {SRC} and dst port {PORT}", prn = lambda x : x.summary())
+match QUESTION:
+   case 0:
+      # Sniff exclusively ICMP packet
+      sniff(filter='icmp', prn = lambda x : x.summary())
+
+   case 1:
+      # Sniff TCP pckets from a specified IP and dest. port of 23
+      SRC:str = "255.255.255.255"
+      PORT:str = "0000"
+      print(f"Sniffing TCP packets from {SRC} destined for port {PORT}")
+      sniff(filter=f"tcp and src {SRC} and dst port {PORT}", prn = lambda x : x.summary())
+
+   case 2:
+      SUBNET:str = "192.168.1"
+      print(f'Capturing packets from / for subnet {SUBNET}')
+      sniff(filter=f"net {SUBNET}", prn = lambda x : x.summary())
